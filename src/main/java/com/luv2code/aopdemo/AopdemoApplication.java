92d8812e8d5554a7d7fdbd64d75cde2ed2fc9c88
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-14 17:47:03 +0100

    add commandLineRuner, reduce logging

--- src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
@@ -10,4 +12,12 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
