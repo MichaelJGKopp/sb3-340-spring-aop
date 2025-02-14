444732a394c8c5d649baeb70dd6d9b2e8d5e4eba
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-14 21:37:01 +0100

    add/use pointcut

--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,12 +13,18 @@ public class MyDemoLoggingAspect {
 
     // WHEN USING * LIMIT RANGE TO PROJECT PACKAGE, NARROW POINT CUT EXPRESSION
 
+
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.add*(..))")  // match package and any number of parameters
+    private void forDaoPackage() {
+    }
+
 //    @Before("execution(public void addAccount())") // match any class
 //    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")  // match specific interface/implementation
 //    @Before("execution(public void add*())")  // match any method starting with add
 //    @Before("execution(* add*(com.luv2code.aopdemo.Account))")  // match specific parameter
 //    @Before("execution(* com.luv2code.aopdemo.dao.*.add*(*))")  // match package and any one parameter
-    @Before("execution(* com.luv2code.aopdemo.dao.*.add*(..))")  // match package and any number of parameters
+//    @Before("execution(* com.luv2code.aopdemo.dao.*.add*(..))")  // match package and any number of parameters
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n===>Executing @Before advice on method");
