62bf2c24e0410c49e08be59cd1c657773a575fb6
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-14 19:02:54 +0100

    match package and any one parameter

--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,7 +13,8 @@ public class MyDemoLoggingAspect {
 //    @Before("execution(public void addAccount())") // match any class
 //    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")  // match specific interface/implementation
 //    @Before("execution(public void add*())")  // match any method starting with add
-    @Before("execution(* add*(com.luv2code.aopdemo.Account))")  // match specific parameter
+//    @Before("execution(* add*(com.luv2code.aopdemo.Account))")  // match specific parameter
+    @Before("execution(* com.luv2code.aopdemo.dao.*.add*(*))")  // match package and any one parameter
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n===>Executing @Before advice on method");
