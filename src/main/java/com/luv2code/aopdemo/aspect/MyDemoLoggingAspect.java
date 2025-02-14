5a8fb1056b544354bebfd205e91109a7b471439e
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-14 19:06:12 +0100

    add warning

--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -10,11 +10,14 @@ public class MyDemoLoggingAspect {
 
     // here are all the advices for logging
 
+    // WHEN USING * LIMIT RANGE TO PROJECT PACKAGE, NARROW POINT CUT EXPRESSION
+
 //    @Before("execution(public void addAccount())") // match any class
 //    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")  // match specific interface/implementation
 //    @Before("execution(public void add*())")  // match any method starting with add
 //    @Before("execution(* add*(com.luv2code.aopdemo.Account))")  // match specific parameter
-    @Before("execution(* com.luv2code.aopdemo.dao.*.add*(*))")  // match package and any one parameter
+//    @Before("execution(* com.luv2code.aopdemo.dao.*.add*(*))")  // match package and any one parameter
+    @Before("execution(* com.luv2code.aopdemo.dao.*.add*(..))")  // match package and any number of parameters
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n===>Executing @Before advice on method");
